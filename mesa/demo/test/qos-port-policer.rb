39cdcb310086c01f3273a599146c4f19f5c86b22
Claus Jensen <claus.jensen@microchip.com>
2026-01-07 15:13:50 +0100

    ET: Port policer test prepared for restructuring

--- mesa/demo/test/qos-port-policer.rb
+++ mesa/demo/test/qos-port-policer.rb
@@ -7,6 +7,61 @@ require_relative 'libeasy/et'
 
 $ts = get_test_setup("mesa_pc_b2b_2x")
 
+# Policer test table
+test_table =
+[
+    {
+        txt: "Policer disabled",
+        cfg: {idx: 0, rate: 0xffffffff},
+        chk: {ig: [0], eg: 1, etolerance: [2]}
+    },
+    {
+        txt: "Policer bit rate 100 kbps",
+        cfg: {idx: 0, rate: 100},
+        chk: {ig: [0], eg: 1, sec: 5, erate: [100000], etolerance: [6], with_pre_tx: true}
+    },
+]
+
+# Policer test function
+def policer_test(t)
+    cfg = fld_get(t, :cfg)
+    chk = fld_get(t, :chk)
+
+    # Get number of policers
+    pol_cnt = $ts.dut.call("mesa_capability", "MESA_CAP_QOS_PORT_POLICER_CNT")
+
+    # Configure port policer
+    port = $ts.dut.p[cfg[:idx]]
+    c = $ts.dut.call("mesa_qos_port_policer_conf_get", port, pol_cnt)
+    pol = c[0]
+    pol["frame_rate"] = fld_get(cfg, :frame_rate, false)
+    pol["policer"]["level"] = fld_get(cfg, :level)
+    pol["policer"]["rate"] = cfg[:rate]
+    $ts.dut.call("mesa_qos_port_policer_conf_set", port, pol_cnt, c)
+
+    # Test policer rate
+    chk[:size] = 1000
+    check_rate(chk)
+end
+
+# Run all or selected test
+sel = table_lookup(test_table, :sel)
+test_table.each do |t|
+    test t[:txt] do
+        next if (t[:sel] != sel)
+        policer_test(t)
+    end
+end
+
+test_summary
+
+test "dump" do
+    #$ts.dut.run("mesa-cmd deb api ai qos")
+end
+
+# ALL BELOW TO BE DELETED
+exit
+
 $pol_cnt = $ts.dut.call("mesa_capability", "MESA_CAP_QOS_PORT_POLICER_CNT")
 
 MESA_BITRATE_DISABLED = 0xffffffff
