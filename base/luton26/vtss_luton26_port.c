25e97aadd4a1f27190ee08a6c942042ec0673135
Bjarni Jonasson <bjarni.jonasson@microchip.com>
2026-03-25 15:52:03 +0100

    MESA-1049: Workaround now only applies to SERDES/Clause-37

--- base/luton26/vtss_luton26_port.c
+++ base/luton26/vtss_luton26_port.c
@@ -102,27 +102,28 @@ vtss_rc vtss_cil_port_clause_37_status_get(vtss_state_t                       *v
 
     /* Get PCS ANEG status register */
     L26_RD(VTSS_DEV_PCS1G_CFG_STATUS_PCS1G_ANEG_STATUS(tgt), &value);
-    L26_RD(VTSS_DEV_PCS1G_CFG_STATUS_PCS1G_ANEG_CFG(tgt), &aneg_cfg);
 
     /* Get 'Aneg complete'   */
     status->autoneg.complete = L26_BF(DEV_PCS1G_CFG_STATUS_PCS1G_ANEG_STATUS_ANEG_COMPLETE, value);
-
-    /* Aneg restart workaround for for 1000Base-X / SGMII-CISCO (TN1395)                       */
-    /* Case-1: ANEG state machine completes with no capabilities (ignore ACK bit 14)  */
-    /* Case-2: ANEG state machine does not complete despite being in Sync             */
-    if (aneg_cfg & VTSS_F_DEV_PCS1G_CFG_STATUS_PCS1G_ANEG_CFG_ANEG_ENA) {
-        if ((status->autoneg.complete && (((value >> 16) & 0xbfff) == 0)) ||
-            (!status->autoneg.complete && in_sync)) {
-            L26_WRM_CLR(VTSS_DEV_PCS1G_CFG_STATUS_PCS1G_CFG(tgt),
-                        VTSS_F_DEV_PCS1G_CFG_STATUS_PCS1G_CFG_PCS_ENA);
-            L26_WRM_SET(VTSS_DEV_PCS1G_CFG_STATUS_PCS1G_CFG(tgt),
-                        VTSS_F_DEV_PCS1G_CFG_STATUS_PCS1G_CFG_PCS_ENA);
-            /* Restart Aneg */
-            (void)vtss_cil_port_clause_37_ctrl_set(vtss_state, port_no);
-            VTSS_MSLEEP(50);
-            L26_RD(VTSS_DEV_PCS1G_CFG_STATUS_PCS1G_ANEG_STATUS(tgt), &value);
-            status->autoneg.complete =
-                L26_BF(DEV_PCS1G_CFG_STATUS_PCS1G_ANEG_STATUS_ANEG_COMPLETE, value);
+    if (vtss_state->port.conf[port_no].if_type == VTSS_PORT_INTERFACE_SERDES) {
+        /* Aneg restart workaround for for 1000Base-X  (TN1395)                           */
+        /* Case-1: ANEG state machine completes with no capabilities (ignore ACK bit 14)  */
+        /* Case-2: ANEG state machine does not complete despite being in Sync             */
+        L26_RD(VTSS_DEV_PCS1G_CFG_STATUS_PCS1G_ANEG_CFG(tgt), &aneg_cfg);
+        if (aneg_cfg & VTSS_F_DEV_PCS1G_CFG_STATUS_PCS1G_ANEG_CFG_ANEG_ENA) {
+            if ((status->autoneg.complete && (((value >> 16) & 0xbfff) == 0)) ||
+                (!status->autoneg.complete && in_sync)) {
+                L26_WRM_CLR(VTSS_DEV_PCS1G_CFG_STATUS_PCS1G_CFG(tgt),
+                            VTSS_F_DEV_PCS1G_CFG_STATUS_PCS1G_CFG_PCS_ENA);
+                L26_WRM_SET(VTSS_DEV_PCS1G_CFG_STATUS_PCS1G_CFG(tgt),
+                            VTSS_F_DEV_PCS1G_CFG_STATUS_PCS1G_CFG_PCS_ENA);
+                /* Restart Aneg */
+                (void)vtss_cil_port_clause_37_ctrl_set(vtss_state, port_no);
+                VTSS_MSLEEP(50);
+                L26_RD(VTSS_DEV_PCS1G_CFG_STATUS_PCS1G_ANEG_STATUS(tgt), &value);
+                status->autoneg.complete =
+                    L26_BF(DEV_PCS1G_CFG_STATUS_PCS1G_ANEG_STATUS_ANEG_COMPLETE, value);
+            }
         }
     }
 
