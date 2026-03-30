91db601f4a688d9e497537267fa3ffd8994c0969
Charles Perry <charles.perry@microchip.com>
2026-03-30 09:45:07 -0700

    LK: add FEATURE_PSFP and FEATURE_TIMESTAMP
    Those are two core features of TSN supported by Laika.

--- base/fa/vtss_fa.c
+++ base/fa/vtss_fa.c
@@ -2603,6 +2603,8 @@ static vtss_rc fa_feature_init(vtss_state_t *vtss_state)
     case VTSS_TARGET_P64H:
         vtss_state->vtss_features[FEATURE_QOS_FRAME_PREEMPTION] = TRUE;
         vtss_state->vtss_features[FEATURE_FRER] = TRUE;
+        vtss_state->vtss_features[FEATURE_PSFP] = TRUE;
+        vtss_state->vtss_features[FEATURE_TIMESTAMP] = TRUE;
         break;
     case VTSS_TARGET_7546:
     case VTSS_TARGET_7549:
