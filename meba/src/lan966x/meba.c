8a5d7c706566c49db35515546e7df24bb74ed0a3
Palle Christensen <palle.christensen@microchip.com>
2026-05-05 13:40:57 +0200

    Fix module detection for EV58G16A

--- meba/src/lan966x/meba.c
+++ meba/src/lan966x/meba.c
@@ -190,6 +190,8 @@ static port_map_t port_table_eds2_lan8842[] = {
     {1, 0, MESA_MIIM_CONTROLLER_1, 2, MESA_PORT_INTERFACE_SGMII, MEBA_CAP_INT_LAN8814,           false, 1},
     {3, 0, MESA_MIIM_CONTROLLER_0, 0, MESA_PORT_INTERFACE_SGMII, MEBA_PORT_CAP_TRI_SPEED_COPPER,
      false,                                                                                             1},
+    {4, 0, MESA_MIIM_CONTROLLER_0, 2, MESA_PORT_INTERFACE_SGMII, MEBA_PORT_CAP_TRI_SPEED_COPPER,
+     false,                                                                                             1},
 };
 
 #define MEBA_CAP_LAN8870 (MEBA_PORT_CAP_TRI_SPEED_COPPER & ~MEBA_PORT_CAP_AUTONEG)
@@ -1129,7 +1131,7 @@ const char *ev16r73a = "EV16R73A";
 const char *ev12n54a = "EV12N54A";
 const char *ev87s66a = "EV87S66A";
 const char *ev42y23a = "EV42Y23A";
-const char *ev58g16a = "EV58G16A";
+const char *ev58g16a = "EV58316A";
 
 mesa_rc read_plugin_module(meba_inst_t inst, int address, const char **plugin_module)
 {
@@ -1315,7 +1317,7 @@ meba_inst_t meba_initialize(size_t callouts_size, const meba_board_interface_t *
                                     port_table_eds2_lan8870);
         } else if (plugin_module == ev58g16a) {
             // Using the builtin PHYs + lan8842 in first slot
-            inst->props.mux_mode = MESA_PORT_MUX_MODE_1;
+            inst->props.mux_mode = MESA_PORT_MUX_MODE_5;
             lan966x_init_port_table(inst, sizeof(port_table_eds2_lan8842) / sizeof(port_map_t),
                                     port_table_eds2_lan8842);
         } else {
