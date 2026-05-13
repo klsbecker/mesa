e871baf0a69645af6f683fcef2558a986c726af2
Horatiu Vultur <horatiu.vultur@microchip.com>
2026-05-13 10:38:10 +0200

    mepa: ksz: Remove unused includes
    Remove the unused includes

--- mepa/ksz9031/src/ksz9031.c
+++ mepa/ksz9031/src/ksz9031.c
@@ -1,11 +1,6 @@
 // Copyright (c) 2004-2020 Microchip Technology Inc. and its subsidiaries.
 // SPDX-License-Identifier: MIT
 
-#include <unistd.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <stdbool.h>
-
 #include <microchip/ethernet/phy/api.h>
 #include <mepa_driver.h>
 #include <mepa_ts_driver.h>
